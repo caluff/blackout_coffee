f5e43214a81f78e8d79234300795391c228ef58b
Caluff <dcaluff@gmail.com>
2023-08-04 18:46:56 -0400

    update panel de photo

--- app/offer/@admin/page.tsx
+++ app/offer/@admin/page.tsx
@@ -10,7 +10,7 @@ const Admin = () => {
         <form action="#">
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
-              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-white leading-6">
                 Nombre del Producto
               </label>
               <input
@@ -25,7 +25,7 @@ const Admin = () => {
             <div className="w-full">
               <label
                 htmlFor="category"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                className="block mb-2 text-sm font-medium text-white leading-6">
                 Category
               </label>
               <select
@@ -44,7 +44,7 @@ const Admin = () => {
             <div className="w-full">
               <label
                 htmlFor="price"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                className="block mb-2 text-sm font-medium text-white leading-6">
                 Precio
               </label>
               <div className={"relative"}>
@@ -64,7 +64,7 @@ const Admin = () => {
             <div className="sm:col-span-2">
               <label
                 htmlFor="description"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                className="block mb-2 text-sm font-medium text-white leading-6">
                 Descripción
               </label>
               <textarea
@@ -76,39 +76,31 @@ const Admin = () => {
 
               </textarea>
             </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="description"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <div className="col-span-full">
+              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-white">
                 Imagen del producto
               </label>
-              <div className={"flex justify-center items-center w-full"}>
-                <label
-                  className={"rounded-lg flex flex-col justify-center items-center w-full h-64 border-2 border-dashed cursor-pointer hover:bg-gray-600 bg-gray-700 border-gray-600"}>
-                  <div className={"pb-6 pt-5 justify-center items-center flex-col flex"}>
-                    <CloudArrowUpIcon className="h-10 w-10 text-gray-400 mb-3 block align-middle"/>
-                    <p className={"text-sm leading-5 mb-2 text-gray-400"}>
-                      <span className={"font-semibold"}>
-                        Click to upload
-                      </span> or drag and drop
-                    </p>
-                    <p className={"mb-4 text-gray-400 text-xs leading-4"}>
-                      Max. File Size: 1mb
-                    </p>
-                    <button
-                      type={"button"}
-                      className={"text-white bg-primary-600 font-medium text-xs leading-4 items-center px-3 py-2 rounded-lg focus:ring-primary-900 hover:bg-primary-800"}
+              <div className="group mt-2 flex justify-center rounded-lg border border-dashed border-gray-600 px-6 py-10 bg-gray-700 hover:bg-gray-600">
+                <div className="text-center">
+                  <CloudArrowUpIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true"/>
+                  <div className="mt-4 flex text-sm leading-6 text-gray-300">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-gray-700 font-semibold text-indigo-400 group-hover:bg-gray-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                     >
-                      Extra Small
-                    </button>
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" className="sr-only"/>
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
                   </div>
-                </label>
+                  <p className="text-xs leading-5 text-gray-400">PNG, JPG, GIF up to 1MB</p>
+                </div>
               </div>
             </div>
           </div>
           <button
             type="submit"
-            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-900 hover:bg-primary-800">
+            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-indigo-500 rounded-lg focus:ring-4 focus:ring-indigo-900 hover:bg-indigo-700">
             Añadir producto
           </button>
         </form>
