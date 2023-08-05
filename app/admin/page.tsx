24359a7c0d9cabd59ae2df81af9a95aa94dccbe0
Caluff <dcaluff@gmail.com>
2023-08-04 20:54:39 -0400

    update admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Image from "next/image";
+import bg_admin from '@/public/bg_admin.jpg'
 const Admin = () => {
   return (
-    <div>
-      Admin
+    <div className={"h-screen w-screen flex overflow-hidden"}>
+      <Image
+        className={"hidden h-screen w-3/6 md:block"}
+        src={bg_admin}
+        alt={"bg admin"}/>
     </div>
   );
 };
